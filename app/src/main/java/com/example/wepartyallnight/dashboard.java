6fc9cb7a7614a40ff3bd183de5494ad6eecd148d
Asthino Zafiamy <asthino.zafiamy@sogefi-sig.com>
2022-06-10 13:46:43 +0200

    [E] - Action swith page

--- app/src/main/java/com/example/wepartyallnight/dashboard.java
+++ app/src/main/java/com/example/wepartyallnight/dashboard.java
@@ -2,13 +2,31 @@ package com.example.wepartyallnight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class dashboard extends AppCompatActivity {
+    private Button btn_new_event;
+    private Button btn_envents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        btn_envents = (Button) findViewById(R.id.btn_new_event);
+
+        btn_envents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage(list_events.class);
+            }
+        });
+    }
+    private void openPage(Class<?> page) {
+        Intent switchActivityIntent = new Intent(this, page);
+        startActivity(switchActivityIntent);
     }
 }
