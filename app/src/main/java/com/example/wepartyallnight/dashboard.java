a6a46e3b93784c4a2b1e441b90ede16df9b933d6
Asthino Zafiamy <asthino.zafiamy@sogefi-sig.com>
2022-06-10 16:30:37 +0200

    dsqhfsd

--- app/src/main/java/com/example/wepartyallnight/dashboard.java
+++ app/src/main/java/com/example/wepartyallnight/dashboard.java
@@ -11,14 +11,22 @@ import android.widget.Button;
 public class dashboard extends AppCompatActivity {
     private Button btn_new_event;
     private Button btn_envents;
+    private Button btn_n_envents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         btn_envents = (Button) findViewById(R.id.btn_list_events);
+        btn_n_envents = (Button) findViewById(R.id.btn_new_event);
 
         btn_envents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPage(list_events.class);
+            }
+        });
+        btn_n_envents.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openPage(list_events.class);
